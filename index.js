e83cfe14f0ed8b3279e5da7a945c890ba006b7cd
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2022-01-25 16:53:55 -0700

    setup;

---
+++ index.js
@@ -0,0 +1,34 @@
+var addButton = document.getElementById('add');
+var errorMessage = document.getElementById('error-message')
+
+const validateInput = () => {
+    var name = document.getElementById('name').value;
+    var interval = document.getElementById('interval').value;
+    var times = document.getElementById('times').value;
+
+    if(!name || name === '') {
+        return 'name cannot be empty'
+    }
+    if(isNaN(interval) || interval <= 0) {
+        return 'interval must be a positive integer'
+    }
+    if(isNaN(times) || times <= 0) {
+        return 'times must be a positive integer'
+    }
+
+    return {
+        name: name,
+        interval: interval,
+        times: times
+    }
+}
+
+addButton.addEventListener('click', (e) => {
+    errorMessage.innerHTML = '';
+    var input = validateInput();
+    if (typeof input === 'string') {
+        errorMessage.innerHTML = input;
+    } else {
+        console.log(input)
+    }
+})
